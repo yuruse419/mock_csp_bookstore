4087eebbe688f7c2102fd032dad28e87ab7f5cd6
yuruse419 <chaseorion19@gmail.com>
2024-01-16 17:36:14 -0600

    added comments

--- src/components/footer/FooterComponent.jsx
+++ src/components/footer/FooterComponent.jsx
@@ -4,7 +4,10 @@ import '../../css/footer/FooterComponent.css'
 class FooterComponent extends React.Component {
   render() {
     return (
-      <footer id='page-footer'></footer>
+      <footer id='page-footer'>
+        <p>Created by Chase Shertzer (shertzec@csp.edu)</p>
+        <p>1/16/2024</p>
+      </footer>
     )
   }
 }
--- src/components/main/LoginComponent.jsx
+++ src/components/main/LoginComponent.jsx
@@ -2,46 +2,61 @@ import React from 'react'
 import '../../css/main/LoginComponent.css'
 
 class LoginComponent extends React.Component {
+  // LoginComponent constructor
   constructor(props) {
     super(props)
 
+    // create username and password states
     this.state = {
       username: '',
       password: ''
     }
-
-    this.handleUsernameOnChange = this.handleUsernameOnChange.bind(this)
-    this.handlePasswordOnChange = this.handlePasswordOnChange.bind(this)
   }
 
-  handleUsernameOnChange(e) {
+  /* 
+    arrow functions have no binding to the this keyword,
+    so explicit binding is not required
+    (i.e., this in an arrow function maintains its reference to the class instance)
+  */
+
+  // username state setter
+  handleUsernameOnChange = (e) => {
     this.setState({ username: e.target.value })
   }
 
-  handlePasswordOnChange(e) {
+  // password state setter
+  handlePasswordOnChange = (e) => {
     this.setState({ password: e.target.value })
   }
 
   render() {
     return(
+      // login main
       <main id='login-main'>
+        {/* authentication form */}
         <form id='login-form'>
+          {/* authentication fieldset */}
           <fieldset id='login-authenticate-fieldset'>
             <legend>Authenticate</legend>
 
+            {/* username input */}
             <label hidden htmlFor='username-input'></label>
             <input id='username-input' placeholder='username' onChange={ this.handleUsernameOnChange }></input>
 
+            {/* password input */}
             <label hidden htmlFor='password-input'></label>
             <input id='password-input' placeholder='password' onChange={ this.handlePasswordOnChange }></input>
           </fieldset>
 
+          {/* submit fieldset */}
           <fieldset id='login-submit-fieldset'>
             <legend>Submit</legend>
 
+            {/* register button */}
             <input id='register-button' type='button' value='Register'></input>
             <label hidden htmlFor='register-button'></label>
 
+            {/* login button */}
             <input id='submit-button' type='submit' value='Login'></input>
             <label hidden htmlFor='submit-button'></label>
           </fieldset>
