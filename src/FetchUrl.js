b114bc388f8103029af0c2bb395e665787ca7607
yuruse419 <chaseorion19@gmail.com>
2024-02-15 22:55:06 -0600

    added server/database

--- src/FetchUrl.js
+++ src/FetchUrl.js
@@ -1,3 +1,3 @@
-const FetchUrl = 'http://localhost:3000' // 'http://localhost:3000' ''mockcspbookstore-production.up.railway.app'
+const FetchUrl = 'mock_csp_bookstore.railway.internal:3000' // 'http://localhost:3000' ''mockcspbookstore-production.up.railway.app' 'mock_csp_bookstore.railway.internal'
 
 export default FetchUrl
